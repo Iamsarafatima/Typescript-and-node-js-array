5b595efb9b17d6378ca990d0ffb0b0ce7fbd0e6e
Iamsarafatima <162489179+Iamsarafatima@users.noreply.github.com>
2024-03-13 12:22:53 -0700

    Add files via upload

---
+++ list.js
@@ -0,0 +1,25 @@
+//Marksheet of a student
+var student = {
+    firstname: "Sara",
+    lastname: "Fatima",
+    fatherName: "Mukhtar Hussain",
+    Age: 25,
+    ClassId: 28764,
+    RollNo: 2020,
+    Subjects: {
+        English: 70,
+        Urdu: 40,
+        Chemistry: 78,
+        Physics: 74,
+        Maths: 90
+    },
+    Annualfee: 5000,
+    Monthlyfee: 2000
+};
+console.log(student.firstname);
+console.log(student.lastname);
+console.log(student.ClassId);
+console.log(student.RollNo);
+console.log(student.Age);
+console.log(student.Subjects);
+console.log(student.Annualfee);
